2f150a543ad70e4002b7042348165ed7d9b0632d
Vinicius de Carvalho Fernandes <114537856+vcfernandes@users.noreply.github.com>
2022-10-24 21:09:47 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
+
+function App() {
+
+  const nome = "Maria"
+
+  return (
+    <div className="App">
+     <SayMyName nome="Vinicius" />
+     <SayMyName nome="João" />
+     <SayMyName nome={nome} />
+     <Pessoa nome="Rodrigo" idade="28" profissao="Programador"
+     foto="https://via.placeholder.com/150"/>
+
+    </div>
+  )
+}
+
+export default App;
